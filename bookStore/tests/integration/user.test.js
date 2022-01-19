da3ec65dfb29b65610289355d714029b6541da53
bharathmighty248 <bharathmighty248@gmail.com>
2022-01-19 11:13:28 +0530

    Add: Test cases are added for Registration Api

--- bookStore/tests/integration/user.test.js
+++ bookStore/tests/integration/user.test.js
@@ -1,10 +1,13 @@
-import { expect } from 'chai';
-import request from 'supertest';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 import mongoose from 'mongoose';
-
 import app from '../../src/index';
 
-describe('User APIs Test', () => {
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Admin APIs Test', () => {
   before((done) => {
     const clearCollections = () => {
       for (const collection in mongoose.connection.collections) {
@@ -26,16 +29,42 @@ describe('User APIs Test', () => {
     done();
   });
 
-  describe('GET /users', () => {
-    it('should return empty array', (done) => {
-      request(app)
-        .get('/api/v1/users')
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.data).to.be.an('array');
+  describe('Admin registration Api', () => {
+    it('GivenRegistrationDetails_WhenProper_shouldReturnSuccessWithRoleAdmin', (done) => {
+      const register = {
+        firstName: "bharath",
+        lastName: "pasumarthi",
+        email: "bharathmighty248@gmail.com",
+        password: "bharath@248"
+      };
+      chai
+      .request(app)
+      .post('/api/v1/users/admin')
+      .send(register)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.have.property("message").eql("Registration successfull");
+        res.body.data.should.have.property("role").eql("Admin");
+        done();
+      });
+    });
 
-          done();
-        });
+    it('GivenRegistrationDetails_WhenEmailAlreadyRegistered_shouldReturnConflict', (done) => {
+      const register = {
+        firstName: "bharath",
+        lastName: "pasumarthi",
+        email: "bharathmighty248@gmail.com",
+        password: "bharath@248"
+      };
+      chai
+      .request(app)
+      .post('/api/v1/users/admin')
+      .send(register)
+      .end((err, res) => {
+        res.should.have.status(409);
+        res.body.should.have.property("message").eql("Email Already Exist");
+        done();
+      });
     });
   });
 });
